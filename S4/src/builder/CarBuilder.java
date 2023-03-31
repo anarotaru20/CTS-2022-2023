a7ae21f67664e984bed151e56da4e60e5db8cc1d
anarotaru20 <anarotaru20@gmail.com>
2023-03-31 19:24:08 +0300

    S4 - Builder & Prototype

---
+++ S4/src/builder/CarBuilder.java
@@ -0,0 +1,13 @@
+package builder;
+
+public interface CarBuilder {
+    public void buildChassis();
+
+    public void buildWheels();
+
+    public void buildInteriorDesign();
+
+    public void buildEngine();
+
+    public Car getCar();
+}
